0e6fc8a36c4499a39408df1806170ae3a4b6b14a
Stephen BOLIVAR <bolivar.stepen@gmail.com>
2024-11-23 07:24:18 -0400

    fix: add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImouModule } from './imou/imou.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppLoggerMiddleware } from './_middlewares/app-logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), ImouModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AppLoggerMiddleware).forRoutes('*');
+  }
+}
